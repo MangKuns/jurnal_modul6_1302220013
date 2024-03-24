6cb7f32aebfc8500862a06c47419cc1410cd8e9a
unknown <kusumarizky65@gmail.com>
2024-03-25 06:42:12 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modul6_1302220013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
